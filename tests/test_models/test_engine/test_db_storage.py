f13014b520eaca012d2238099487dd9338b12bad
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-20 19:19:39 +0300

    My commit

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -33,14 +33,14 @@ class TestDBStorageDocs(unittest.TestCase):
     def test_pep8_conformance_db_storage(self):
         """Test that models/engine/db_storage.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files([models/engine/db_storage.py])
+        result = pep8s.check_files('[models/engine/db_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_db_storage(self):
         """Test tests/test_models/test_db_storage.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files([tests/test_models/test_engine/test_db_storage.py])
+        result = pep8s.check_files(['tests/test_models/test_engine/test_db_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
@@ -69,12 +69,12 @@ class TestDBStorageDocs(unittest.TestCase):
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
-    @unittest.skipIf(models.storage_t != db, "not testing db storage")
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
         self.assertIs(type(models.storage.all()), dict)
 
-    @unittest.skipIf(models.storage_t != db, "not testing db storage")
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
 
@@ -82,7 +82,7 @@ class TestFileStorage(unittest.TestCase):
     def test_new(self):
         """test that new adds an object to the database"""
 
-    @unittest.skipIf(models.storage_t != db, "not testing db storage")
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
@@ -90,7 +90,7 @@ class TestFileStorage(unittest.TestCase):
 class TestDBStorage(unittest.TestCase):
     """Test the DBStorage class"""
 
-    @unittest.skipIf(os.getenv(HBNB_TYPE_STORAGE) != db,
+    @unittest.skipIf(os.getenv(HBNB_TYPE_STORAGE) != 'db',
                      "not testing db storage")
     def test_get(self):
         """Test that get returns specific object, or none"""
@@ -103,7 +103,7 @@ class TestDBStorage(unittest.TestCase):
         self.assertIs(None, models.storage.get("blah", "blah"))
         self.assertIs(new_user, models.storage.get("User", new_user.id))
 
-    @unittest.skipIf(os.getenv(HBNB_TYPE_STORAGE) != db,
+    @unittest.skipIf(os.getenv(HBNB_TYPE_STORAGE) != 'db',
                      "not testing db storage")
     def test_count(self):
         """test that new adds an object to the database"""
