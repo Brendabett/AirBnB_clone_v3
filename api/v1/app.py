65e4eaac71e52a2e54b08ba8ebb731d4ce7cb5d2
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-20 19:50:01 +0300

    My commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,17 +14,17 @@ app.config[SWAGGER] = {
     "swagger_version": "2.0",
     "title": "Flasgger",
     "headers": [
-        (Access-Control-Allow-Origin, *),
-        (Access-Control-Allow-Methods, "GET, POST, PUT, DELETE, OPTIONS"),
-        (Access-Control-Allow-Credentials, "true"),
+        ('Access-Control-Allow-Origin', *),
+        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
+        ('Access-Control-Allow-Credentials', "true"),
     ],
     "specs": [
         {
             "version": "1.0",
             "title": "HBNB API",
-            "endpoint": v1_views,
+            "endpoint": 'v1_views',
             "description": HBNB REST API,
-            "route": /v1/views,
+            "route": '/v1/views',
         }
     ]
 }
@@ -43,10 +43,10 @@ def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     HBNB_API_HOST = getenv(HBNB_API_HOST)
     HBNB_API_PORT = getenv(HBNB_API_PORT)
 
-    host = 0.0.0.0 if not HBNB_API_HOST else HBNB_API_HOST
+    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
     port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
     app.run(host=host, port=port, threaded=True)
