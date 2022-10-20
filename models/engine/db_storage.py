4e4ce1b9da23968056cc13628511a3bb3429f016
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-20 18:49:08 +0300

    My commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,12 +27,12 @@ class DBStorage:
 
     def __init__(self):
         """Instantiate a DBStorage object"""
-        HBNB_MYSQL_USER = getenv(HBNB_MYSQL_USER)
-        HBNB_MYSQL_PWD = getenv(HBNB_MYSQL_PWD)
-        HBNB_MYSQL_HOST = getenv(HBNB_MYSQL_HOST)
-        HBNB_MYSQL_DB = getenv(HBNB_MYSQL_DB)
-        HBNB_ENV = getenv(HBNB_ENV)
-        self.__engine = create_engine(mysql+mysqldb://{}:{}@{}/{}.
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
                                              HBNB_MYSQL_HOST,
@@ -47,7 +47,7 @@ class DBStorage:
             if cls is None or cls is classes[clss] or cls is clss:
                 objs = self.__session.query(classes[clss]).all()
                 for obj in objs:
-                    key = obj.__class__.__name__ + . + obj.id
+                    key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
         return (new_dict)
 
