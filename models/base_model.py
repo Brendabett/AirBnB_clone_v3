c9776a198bad694acf50d7059374c1b070595155
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-20 14:59:56 +0300

    My commit

--- models/base_model.py
+++ models/base_model.py
@@ -54,7 +54,7 @@ class BaseModel:
                                          self.__dict__)
 
     def save(self):
-        """updates the attribute updated_at with the current datetime"""
+        """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
