aa56253f14183be5cfdad3cd730b0f3947747d95
Brendabett <brendabett33@gmail.com>
2022-10-20 15:36:46 +0000

    My commit

--- models/user.py
+++ models/user.py
@@ -6,19 +6,19 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    if models.storage_t == db:
+        __tablename__ = users
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
+
     else:
         email = ""
         password = ""
@@ -27,9 +27,4 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
-        if password in kwargs:
-            password = kwargs['password']
-            m = hashlib.md5()
-            m.update(str.encode(password))
-            kwargs[password] = m.hexdigest()
         super().__init__(*args, **kwargs)
