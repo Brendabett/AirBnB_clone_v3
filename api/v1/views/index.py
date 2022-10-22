ab23e7302a7036fd8f9272e71387d6e5f49ec792
Brendabett <brendabett33@gmail.com>
2022-10-22 17:40:40 +0000

    My commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-""" Index """
-from api.v1.views import app_views
+"""view module"""
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Returns JSON """
-    return jsonify(status="OK")
+    """handles /status route"""
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    """ Returns the number of each instance type """
-    return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+    """handles /stats route"""
+    object_count = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(object_count)
